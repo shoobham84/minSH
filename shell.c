0c7d4f039868923c42530f8130f92ba694213ab9
shoobham84 <shubhamnayak392reborn@gmail.com>
2026-04-22 21:37:34 +0530

    Add support of external input using isatty()

--- shell.c
+++ shell.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main() {
+    int interactive = isatty(STDIN_FILENO);
+
     while(1) {
-        printf("min> ");
+        if (interactive) {
+            printf("min> ");
+            fflush(stdout); 
+        }
+        
         char *buff = NULL;
         size_t len = 0;
+        
+        // basic getline instead of read syscall
         if (getline(&buff, &len, stdin) == -1) {
-            printf("\n");
+            if (interactive) {
+                printf("\n");
+            }
+            free(buff);
             break;
         }
+        free(buff); 
     }
+    
+    return 0;
 }
