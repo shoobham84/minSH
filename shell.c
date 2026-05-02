1263d9fa7fb6c8ae728eb42960ee294937cbce57
shoobham84 <shubhamnayak392reborn@gmail.com>
2026-05-03 01:01:14 +0530

    Refactor shell loop mechanics for semantics by using shell_init var

--- shell.c
+++ shell.c
@@ -5,12 +5,13 @@
 #include "command.h"
 
 int interactive = 0;
+int shell_init = 1;
 
 int main() {
     interactive = isatty(STDIN_FILENO);
 
     /* int shell_init = 0;   upon sighandling */
-    while(1) {
+    while(shell_init) {
         if (interactive) {
             printf("min> ");
             fflush(stdout); 
@@ -19,7 +20,7 @@ int main() {
         char *buff = NULL;
         size_t len = 0;
         
-        // basic getline instead of read syscall
+        /* basic getline instead of read syscall */
         if (getline(&buff, &len, stdin) == -1) {
             if (interactive) {
                 printf("\n");
@@ -27,7 +28,7 @@ int main() {
             free(buff);
             break;
         }
-        
+        // printf("%s", buff);
         free(buff); 
     }
     
